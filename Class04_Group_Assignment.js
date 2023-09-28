348c84e6ec8bf24454efa033fdc52e4a22a9d8b2
acksll <146391405+acksll@users.noreply.github.com>
2023-09-28 14:45:36 -0700

    Add files via upload

---
+++ Class04_Group_Assignment.js
@@ -0,0 +1,213 @@
+// Use camel Case as needed
+
+/** Question 1
+ * initialise the variable x to the value of 0
+ * console log the variable x with a preincrement. Write a single line comment as to what that output will be
+ * console log the variable x with a postincrement. Write a single line comment as to what that output will be
+ * console log the variable x. Write a single line comment as to what that output will be
+ */
+var x = 0;
+console.log(++x); //1
+console.log(x++); //1
+console.log(x) //2
+
+
+
+/** Question 2
+ * initialise the variable y to the value of 10
+ * console log the variable y with a postdecrement. Write a single line comment as to what that output will be
+ * console log the variable y with a predecrement. Write a single line comment as to what that output will be
+ */
+var y = 10;
+console.log(y--);
+console.log(--y);
+
+
+
+
+
+/** Question 3
+ * initialise the variable z to the value of 50
+ * decrement the variable z by 5
+ * increment the variable z by 1
+ * decrement the variable z by 14
+ * console log the variable z
+ */
+var z = 50;
+z = z - 5
+z++
+z -= 14
+
+
+
+
+/** Question 4
+ * initialise the variable `bag one` to the value of 10
+ * increment bag one by 2
+ * initialise the variable `bag two` to the value of 12
+ * decrement bag two by 2
+ * initialise total to the average of bag one and bag two
+ * console log the total. Write a single line comment as to what will be displayed
+ */
+var bagOne = 10;
+bagOne += 2;
+var bagTwo = 12;
+bagTwo -= 2;
+var total = (bagOne + bagTwo)/2;
+console.log(total)
+
+
+
+
+/** Question 5
+ * initialise the variable `container one` to the value of 50
+ * increment container one by 5
+ * initialise the variable `container two` to the value of 20
+ * decrement container two by 10
+ * initialise total to the average of container one and container two
+ * console log the total. Write a single line comment as to what will be displayed
+ */
+var containerOne = 50;
+containerOne += 5;
+var containerTwo = 20;
+containerTwo -=10;
+var total = (containerOne + containerTwo)/2;
+console.log(total)
+
+
+
+
+
+/** Question 6
+ * initialise the variable `litre one` to the value of 1.5
+ * initialise the variable `litre two` to the value of 2
+ * initialise the variable `litre three` to the value of 5.5
+ * initialise total to the average of litre one, litre two and litre three
+ * console log the total. Write a single line comment as to what will be displayed
+ */
+var litreOne = 1.5;
+var litreTwo = 2;
+var litreThree = 5.5;
+total = (litreOne + litreTwo + litreThree)/3;
+console.log(total)
+
+
+
+
+
+/** Question 7
+ * initialise the variable `centimeter one` to the value of 6
+ * initialise the variable `centimeter two` to the value of 3
+ * initialise the variable `centimeter three` to the value of 8
+ * initialise total to the average of centimeter one, centimeter two and centimeter three
+ * console log the total and only show 2 decimal places using the .toFixed method. Write a single line comment as to what will be displayed
+ */
+var centimetreOne = 6;
+var centimetreTwo = 3;
+var centimetreThree = 8;
+var total = (centimetreOne + centimetreTwo + centimetreThree)/3;
+console.log(total.toFixed(2))
+
+
+
+
+
+/** Question 8
+ * initialise the variable `fan level one` to the value of 8
+ * initialise the variable `fan level two` to the value of 4
+ * initialise the variable `fan level three` to the value of 14
+ * initialise total to the average of fan level one, fan level two and fan level three
+ * console log the total to 3 decimal place. Write a single line comment as to what will be displayed
+ */
+var fanlevelOne = 8;
+var fanlevelTwo = 4;
+var fanlevelThree = 14
+total = (fanlevelOne + fanlevelTwo + fanlevelThree)/2;
+console.log(toFixed(2));
+
+
+
+/** Question 9
+ * initialise the variable `case one` to the value of 10
+ * initialise the variable `case two` to the value of 30
+ * initialise the variable `case three` to the value of 35
+ * initialise total to the average of case one, case two and case three
+ * console log the total and round the total to 1 decimal places. Write a single line comment as to what will be displayed
+ */
+var caseOne = 10;
+var caseTwo = 30;
+var caseThree = 35;
+total = (caseOne + caseTwo + caseThree);
+
+
+/** Question 10
+ * initialise the variable `guitar strings` to the value of 6
+ * initialise the variable `violin strings` to the value of 4
+ * initialise total to the average of guitar strings and violin strings
+ * console log the total and round the total to 1 decimal place
+ */
+var guitarStrings = 6;
+var violinStrings = 4;
+var total = (guitarStrings + violinStrings)/2;
+console.log (total.toFixed(1))
+
+
+
+/** Question 11
+ * initialise the variable `box one` to the value of 3
+ * initialise the variable `box two` to the value of 4
+ * initialise the variable `box three` to the value of 6
+ * initialise the variable `box four` to the value of 9
+ * initialise total to the average of all the boxes
+ * console log the total. Write a single line comment as to what will be displayed
+ */
+var boxOne = 3;
+var boxTwo = 4;
+var boxThree = 6;
+var boxFour = 9;
+var total = (boxOne + boxTwo + boxThree + boxFour)/4;
+console.log(total);
+
+
+
+
+// Question 12 - write your own arithmatic question with incrementing + answer
+/**
+ * initialize the variable apple to the value of 5
+ * increase the variable apple by 1
+ * decrease the variable apple by 1
+ * console log the value of apple.
+ */
+var apple = 5;
+apple +=2;
+apple--;
+console.log(apple);
+
+
+// Question 13 - write your own averaging question + answer
+/**
+ * Initialise variable parnell to the value of 13
+ * Initialise the variable marlo to value of 24
+ * Initialise the variable max to the value of 11
+ * Initialise all names to the avarage of parnell, marlo and max. 
+ * console log the variable all names
+ */
+var parnell = 13;
+var marlo = 24;
+var max = 11;
+var allName = (parnell + marlo + max)/3;
+console.log(allName);
+
+
+// Question 14 - write your own averaging question + answer
+/**
+ * Initialise the variable deluca to the value of 21
+ * Initialise the variable pai to the value of 34
+ * Initialise total to the avarage of all names. 
+ * Console log the variable all names. 
+ */
+
+var deluca = 21;
+var pia = 34;
+var allName = (deluca + pia)/2;
+console.log(AllName);
